b4d0624448c8849079a8bf5de84c6bf018a996f6
annnaaaaaaaaaaaaaaaa <97455434+annnaaaaaaaaaaaaaaaa@users.noreply.github.com>
2022-01-10 16:09:02 +0100

    Update Uhrzeit_Addierer.py
    kommentiert

--- Uhrzeit_Addierer.py
+++ Uhrzeit_Addierer.py
@@ -1,9 +1,13 @@
+# Dieses Programm mit 3 Zahlen und zwar stunden, minuten und mit der Zahl welche dazu addiert werden muss. 
 def uhrzeit_minuten_addieren(stunden, minuten, summand):
+    # Mehr als 60 Minuten werden in Stunden umgerechnet
     while minuten+summand>=60:
         minuten=minuten-60
         stunden=stunden+1
+    # Mehr als 24 Stunden werden in weniger umgerechnet, ganze Tage werden nicht gespeichert.
     while stunden>=24:
         stunden=stunden-24
+    # Die Zeit wird ausgegeben.
     print(str(stunden) +":" + str(minuten+summand))
 
 uhrzeit_minuten_addieren(17, 32, 8) # 17:32 + 8 Min = 17:40
